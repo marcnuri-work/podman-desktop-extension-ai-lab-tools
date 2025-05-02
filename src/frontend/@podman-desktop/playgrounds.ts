951a2693e2f79a1f0648e7270329ca09ce48dff5
Marc Nuri <marc@marcnuri.com>
2025-05-02 09:30:55 +0200

    feat(frontend): playground shows tool results

--- src/frontend/@podman-desktop/playgrounds.ts
+++ src/frontend/@podman-desktop/playgrounds.ts
@@ -11,38 +11,53 @@ export function getPlaygroundConversations(): Conversation[] {
           id: '1-1',
           role: 'system',
           content: 'You are a helpful assistant.',
-          timestamp: Date.now() - 5000,
+          timestamp: Date.now() - 6000,
         } as SystemPrompt,
-        { id: '1-2', role: 'user', content: 'Hello, how are you?', timestamp: Date.now() - 5000 } as UserChat,
+        { id: '1-2', role: 'user', content: 'What can you do?', timestamp: Date.now() - 5500 } as UserChat,
         {
           id: '1-3',
           role: 'assistant',
-          content: 'I am fine, thank you!',
-          completed: Date.now() - 4000,
-          timestamp: Date.now() - 4500,
+          content: 'I can assist you with various tasks like checking the weather.',
+          completed: Date.now() - 4500,
+          timestamp: Date.now() - 5637,
         } as AssistantChat,
-        { id: '1-4', role: 'user', content: 'What can you do?', timestamp: Date.now() - 4000 } as UserChat,
+        {
+          id: '1-4',
+          role: 'user',
+          content: 'Check the weather at Don Benito',
+          timestamp: Date.now() - 4000,
+        } as UserChat,
         {
           id: '1-5',
           role: 'assistant',
-          content: 'I can assist you with various tasks.',
-          completed: Date.now() - 3000,
-          timestamp: Date.now() - 3500,
+          content: {
+            type: 'tool-call', toolCallId: '1-7', toolName: 'weather', args: { location: 'Don Benito' },
+            result: {
+              content: [{type: 'text', text: 'The weather in Don Benito is sunny with a temperature of 25°C.'}],
+            }
+          },
+          completed: Date.now() - 2000,
+          timestamp: Date.now() - 2500,
         } as AssistantChat,
         {
           id: '1-6',
           role: 'user',
-          content: 'Check the weather at Don Benito',
-          timestamp: Date.now() - 3000,
+          content: 'Awesome, now check the weather at Porto',
+          timestamp: Date.now() - 2000,
         } as UserChat,
         {
           id: '1-7',
           role: 'assistant',
-          content: { type: 'tool-call', toolCallId: '1-7', toolName: 'weather', args: { location: 'Don Benito' } },
-          completed: Date.now() - 2000,
-          timestamp: Date.now() - 2500,
+          content: {
+            type: 'tool-call', toolCallId: '1-7', toolName: 'weather', args: { location: 'Porto' },
+          },
+          timestamp: Date.now() - 2000,
         } as AssistantChat,
       ],
+      usage: {
+        completion_tokens: 37,
+        prompt_tokens: 13,
+      }
     },
   ];
 }
