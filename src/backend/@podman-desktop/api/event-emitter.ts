65eda76d6c5505164c748f806da8d21da09a15a1
Marc Nuri <marc@marcnuri.com>
2025-05-07 18:09:51 +0200

    feat: functional EventEmitter

--- src/backend/@podman-desktop/api/event-emitter.ts
+++ src/backend/@podman-desktop/api/event-emitter.ts
@@ -1,13 +1,29 @@
 import {Disposable} from '@podman-desktop/api';
 
 export class EventEmitter<T> {
-  event(listener: (e: T) => any, thisArgs?: any, disposables?: Disposable[]): Disposable {
+
+  private listeners: ((e: T) => any)[] = [];
+  public event = this.eventInternal.bind(this);
+
+  private eventInternal(listener: (e: T) => any, thisArgs?: any, disposables?: Disposable[]): Disposable {
+    if (thisArgs) {
+      listener = listener.bind(thisArgs);
+    }
+    this.listeners.push(listener);
     return Disposable.from(...(disposables ?? []));
   }
+
   fire(data: T): void {
-
+    for (const listener of this.listeners) {
+      try {
+        listener(data);
+      } catch (err) {
+        console.error('Error in event listener', err);
+      }
+    }
   };
-  dispose(): void {
 
+  dispose(): void {
+    this.listeners.length = 0;
   };
 }
